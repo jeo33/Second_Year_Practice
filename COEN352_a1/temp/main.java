59d70eb70b04b4b903e453bfa4fb8bc1161c83a8
junpeng gai <34943011+jeo33@users.noreply.github.com>
2022-11-03 15:28:02 -0400

    Add files via upload

---
+++ COEN352_a1/temp/main.java
@@ -0,0 +1,12 @@
+package Mylist;
+
+public class main {
+	public static void main(String args[])
+	{
+		WarehouseInventory a=new WarehouseInventory();
+		a.init();
+		a.menuDidsplay();
+		a.command(a);
+		System.out.println("Exit!!!!!!!!!!!");
+	}
+}
